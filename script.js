0ee2a395b2cea9ffe888af1abb9c150c24d00f0a
RohitRaj766 <rohitraj2k04@gmail.com>
2025-06-18 19:10:51 +0530

    implemented tic tac toe game logic

---
+++ script.js
@@ -0,0 +1,63 @@
+const msgBox = document.getElementById("msgBox");
+const resetBtn = document.getElementById("resetBtn");
+const indiBoxes = Array.from(document.getElementsByClassName("small--boxes"));
+let userVal = 'X';
+
+// Creating a Toggle Function
+
+function toggle(userVal){
+  return userVal === "X" ? "O" : "X";
+}
+
+
+// when clicked each box have 'O' or 'X'
+
+indiBoxes.forEach(element => {
+    element.addEventListener('click', () => {
+        if (element.innerHTML === "") {
+            element.innerHTML = userVal;
+            checkResult();
+            userVal = toggle(userVal);
+        }
+    });
+});
+
+// resetFunc
+
+resetBtn.addEventListener('click', () => {
+    indiBoxes.forEach(e => {
+        e.innerHTML = "";
+        e.style.pointerEvents = "auto";
+    });
+    msgBox.innerHTML = "";
+    userVal = "X";
+});
+
+// checkResult
+
+function checkResult() {
+    const win = [
+        [0, 1, 2],
+        [1, 4, 7],
+        [0, 3, 6],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+        [3, 4, 5],
+        [6, 7, 8]
+    ];
+
+    win.forEach(pattern => {
+   
+        const [a, b, c] = pattern;
+        const valA = indiBoxes[a].innerHTML;
+        const valB = indiBoxes[b].innerHTML;
+        const valC = indiBoxes[c].innerHTML;
+
+
+        if (valA && valA === valB && valB === valC) {
+            msgBox.innerHTML = `${valB} won!`;
+            indiBoxes.forEach(box => box.style.pointerEvents = "none");
+        }
+    });
+}
